0c5a9f093f2be679592b9e90951cdb5c05c5c5bc
Yash <yashvardhandubey83@gmail.com>
2025-09-26 18:48:05 +0530

    initial update

--- src/api/Index.js
+++ src/api/Index.js
@@ -1,6 +1,6 @@
    const baseURL =
-  "https://api.openweathermap.org/data/2.5/weather";
-  const apikey = "08506024fda1c0d6ffb6a0dfc97d9ac6";
+  "https://api";
+  const apikey = "apikey";
 
 export const getWeatherDataForCity = async (city) => {
   const response = await fetch(`${baseURL}?q=${city}&appid=${apikey}&units=metric`);
@@ -14,6 +14,4 @@ export const getWeatherDataForLocation = async (lat, lon) => {
   return await response.json();
 };
 
-// 08506024fda1c0d6ffb6a0dfc97d9ac6
 
-//https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=08506024fda1c0d6ffb6a0dfc97d9ac6&units=metric
